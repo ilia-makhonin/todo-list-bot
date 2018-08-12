eb21ded9b25c3d01299f01d63d31db3160584e83
Meugenn <meugenn.neg@gmail.com>
2018-08-12 14:49:05 +0300

    1 version

--- main.py
+++ main.py
@@ -1,6 +1,112 @@
-from telebot import TeleBot
+# -*- coding: utf-8 -*-
+
+import telebot
 import config
 import bot_token
 import mysql
 
-bot = TeleBot(bot_token.TOKEN)
+bot = telebot.TeleBot(bot_token.TOKEN)
+
+
+
+
+condition = dict()                  #Состояние пользователей(ожидание ввода и т.д.)
+
+
+business = dict()                   #Список общих дел пользователей
+
+
+
+list_business = dict()              #Списки дел
+
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, config.start_text)
+    condition[message.chat.id] = config.Waiting.STANDART.value          #Пользователь добавляется в БД
+    business[message.chat.id] = []
+    list_business[message.chat.id] = []
+    business_handler(message)
+
+
+edit_business = None
+
+
+@bot.callback_query_handler(func=lambda call: True)  #Ответ на кнопки /business
+def callback_handler(call):
+
+    global edit_business
+    if call.data == "Новое дело":
+
+        bot.send_message(call.from_user.id, 'Введите название дела')
+        condition[call.from_user.id] = config.Waiting.WAITING_BUSINESS.value
+
+    elif call.data == "Удалить дело":
+        markup = config.create_markup(business[call.from_user.id])
+
+        bot.send_message(call.from_user.id, 'Выберите, какое дело вы хотите удалить:', reply_markup=markup)
+        condition[call.from_user.id] = config.Waiting.EDIT_BUSINESS.value
+
+    elif condition[call.from_user.id] == config.Waiting.EDIT_BUSINESS.value:
+        markup = config.create_markup(('Удалить', ))
+        edit_business = call.data
+        bot.send_message(call.from_user.id, 'Вы выбрали дело "{}"'.format(call.data), reply_markup=markup)
+        condition[call.from_user.id] = config.Waiting.EDIT_BUSINESS_WAITING_RESPONSE
+
+    elif condition[call.from_user.id] == config.Waiting.EDIT_BUSINESS_WAITING_RESPONSE:
+        business[call.from_user.id].remove(edit_business)
+        business_handler(call)
+
+
+
+@bot.message_handler(commands=['business'])
+def business_handler(message):
+    try:
+        condition[message.chat.id] = config.Waiting.STANDART.value
+
+        markup = config.create_markup(
+            ("Новое дело", "Удалить дело", "Мои списки дел (не работает)", "Новый список дел (не работает)"))
+
+        if len(business[message.chat.id]) == 0:
+
+             bot.send_message(message.chat.id, config.business_0, reply_markup=markup)
+
+        else:
+
+            bot.send_message(message.chat.id, config.list_of_business(business[message.chat.id]),reply_markup=markup)
+    except:
+        condition[message.from_user.id] = config.Waiting.STANDART.value
+
+        markup = config.create_markup(
+            ("Новое дело", "Удалить дело", "Мои списки дел (не работает)", "Новый список дел (не работает)"))
+
+        if len(business[message.from_user.id]) == 0:
+
+            bot.send_message(message.from_user.id, config.business_0, reply_markup=markup)
+
+        else:
+
+            bot.send_message(message.from_user.id, config.list_of_business(business[message.chat.id]), reply_markup=markup)
+
+
+
+
+@bot.message_handler(func=lambda message: condition[message.chat.id]  == config.Waiting.WAITING_BUSINESS.value)
+def new_business(message):
+    try:
+        business[message.chat.id]
+    except KeyError:
+        business[message.chat.id] = []
+
+    business[message.chat.id].append(message.text)
+    bot.send_message(message.chat.id, 'Отлично, дело добавлено')
+    business_handler(message)
+
+# @bot.message_handler(func=lambda message: condition[message.chat.id]  == config.Waiting.WAITING_LIST.value)
+
+
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
